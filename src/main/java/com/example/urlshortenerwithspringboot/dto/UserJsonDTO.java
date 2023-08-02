938315c23b1651549f7a8d6f1f9bfd88e7d4eb06
Ozan Sahin <Ozan.Sahin@vodafone.com>
2023-08-03 00:52:57 +0300

    Add DTOs

---
+++ src/main/java/com/example/urlshortenerwithspringboot/dto/UserJsonDTO.java
@@ -0,0 +1,35 @@
+package com.example.urlshortenerwithspringboot.dto;
+
+import com.example.urlshortenerwithspringboot.model.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+public class UserJsonDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -2736215277989176542L;
+
+    @JsonProperty("id")
+    private Long id;
+    @JsonProperty("username")
+    private String username;
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("creation_date")
+    private String creationDate;
+
+    public static UserJsonDTO convert(final User user) {
+        UserJsonDTO userJsonDTO = new UserJsonDTO();
+        userJsonDTO.setId(user.getId());
+        userJsonDTO.setUsername(user.getUsername());
+        userJsonDTO.setEmail(user.getEmail());
+        userJsonDTO.setCreationDate(user.getCreationDate());
+        return userJsonDTO;
+    }
+}
