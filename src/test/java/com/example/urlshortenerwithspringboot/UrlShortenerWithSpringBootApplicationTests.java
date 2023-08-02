2aadfd892f4471a353258350d941acc0d64f548a
Ozan Sahin <Ozan.Sahin@vodafone.com>
2023-08-03 00:51:47 +0300

    Add main class

---
+++ src/test/java/com/example/urlshortenerwithspringboot/UrlShortenerWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlshortenerwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
